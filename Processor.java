db1ac164ca8c379e7be77fb531c7e4d069bb5118
TienNguyen <nguyenviettien2021dn@gmail.com>
2026-09-15 18:40:21 +0700

    Add files via upload

---
+++ Processor.java
@@ -0,0 +1,7 @@
+package Lec10_Abstraction.AbstractAccount;
+public class Processor {
+    public static void main(String[] args) {
+        SavingsAccount account = new SavingsAccount(2000);
+        account.display();
+    }
+}
